7a63eaa13c793d4e9faff3e43e3762e47cb52f7a
Joe di Stefano <joeydi@okaypl.us>
2022-10-05 00:57:43 -0400

    add rover-photos

--- data/rovers.js
+++ data/rovers.js
@@ -3,20 +3,24 @@ export const rovers = [
         name: "Curiosity",
         slug: "curiosity",
         image: "rover-curiosity.jpg",
+        cameras: ["FHAZ", "MAHLI", "MARDI", "MAST", "NAVCAM", "RHAZ"],
     },
     {
         name: "Spirit",
         slug: "spirit",
         image: "rover-spirit.jpg",
+        cameras: ["ENTRY", "FHAZ", "NAVCAM", "PANCAM", "RHAZ"],
     },
     {
         name: "Opportunity",
         slug: "opportunity",
         image: "rover-opportunity.jpg",
+        cameras: ["FHAZ", "MINITES", "NAVCAM", "PANCAM", "RHAZ"],
     },
     {
         name: "Perseverance",
         slug: "perseverance",
         image: "rover-perseverance.jpg",
+        cameras: ["EDL_DDCAM", "EDL_PUCAM2", "EDL_RDCAM", "EDL_RUCAM", "FRONT_HAZCAM_LEFT_A", "FRONT_HAZCAM_RIGHT_A", "MCZ_LEFT", "MCZ_RIGHT", "NAVCAM_LEFT", "NAVCAM_RIGHT", "REAR_HAZCAM_LEFT", "REAR_HAZCAM_RIGHT", "SKYCAM"],
     },
 ];
--- pages/[slug].js
+++ pages/[slug].js
@@ -1,4 +1,7 @@
+import { useCallback, useEffect, useState } from "react";
 import Image from "next/future/image";
+import debounce from "underscore/modules/debounce.js";
+
 import { rovers } from "../data/rovers.js";
 
 export default function RoverDetails({ rover, manifest }) {
@@ -12,11 +15,39 @@ export default function RoverDetails({ rover, manifest }) {
         { name: "Total Photos", key: "total_photos" },
     ];
 
+    const [activeSol, setActiveSol] = useState(1);
+    const [activeCamera, setActiveCamera] = useState();
+    const [photos, setPhotos] = useState([]);
+
+    const searchParams = new URLSearchParams();
+    activeSol && searchParams.set("sol", activeSol);
+    activeCamera && searchParams.set("camera", activeCamera);
+
+    const endpoint = `https://mars-photos.herokuapp.com/api/v1/rovers/${rover.slug}/photos?${searchParams.toString()}`;
+
+    useEffect(() => {
+        fetch(endpoint)
+            .then((response) => response.json())
+            .then((data) => {
+                setPhotos(data.photos);
+            });
+    }, [activeSol, activeCamera]);
+
+    const handleSolChange = function (e) {
+        setActiveSol(e.target.value);
+    };
+
+    const debouncedSolChangeHandler = useCallback(debounce(handleSolChange, 500), []);
+
+    const handleCameraChange = function (e) {
+        setActiveCamera(e.target.value);
+    };
+
     return (
         <div className="rover-details">
             <div className="rover-hero">
                 <div className="rover-hero-image">
-                    <Image src={`/images/${rover.image}`} width="3000" height="2000" layout="responsive" />
+                    <Image src={`/images/${rover.image}`} width="3000" height="2000" alt="" />
                 </div>
                 <div className="rover-hero-content">
                     <h1>{rover.name}</h1>
@@ -30,11 +61,41 @@ export default function RoverDetails({ rover, manifest }) {
                     </div>
                 </div>
             </div>
+            <div className="rover-photos">
+                <div className="sol-select">
+                    <span>Sol {rover.max_sol}</span>
+                    <input type="range" min="1" max={rover.max_sol} step="1" xxxvalue={activeSol} onChange={debouncedSolChangeHandler} />
+                </div>
+                <div className="camera-select">
+                    <span>Camera</span>
+                    <div className="checkbox-pills">
+                        <div className="checkbox">
+                            <input id="all" type="checkbox" name="camera" value="" checked={!activeCamera} onChange={handleCameraChange} />
+                            <label htmlFor="all">All</label>
+                        </div>
+                        {rover.cameras.map((camera) => {
+                            return (
+                                <div key={camera} className="checkbox">
+                                    <input id={camera} type="checkbox" name="camera" value={camera} checked={camera === activeCamera} onChange={handleCameraChange} />
+                                    <label htmlFor={camera}>{camera}</label>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className="rover-photos-grid">
+                    {photos.map((photo) => (
+                        <div className="photo">
+                            <img key={photo.id} src={photo.img_src} alt="" loading="lazy" />
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
 
-export async function getStaticPaths({ params }) {
+export async function getStaticPaths() {
     const paths = rovers.map((rover) => ({
         params: { slug: rover.slug },
     }));
